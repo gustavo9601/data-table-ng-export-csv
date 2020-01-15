21930aa003288521201c7bb086e48960fff233d5
Gustavo Adolfo Marquez Prieto <gustavomp@siju.local>
2020-01-15 16:02:50 -0500

    Data table, con filter, expand de datos y exportando a csv

---
+++ src/app/app.component.ts
@@ -0,0 +1,163 @@
+import { Component, OnInit, ElementRef, HostListener, AfterViewInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { MdbTableDirective, MdbTablePaginationComponent } from 'angular-bootstrap-md'; 
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
+  @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
+  @ViewChild('row', { static: true }) row: ElementRef;
+
+
+  elements: any = [
+    {
+      id: 1,
+      first: 'Mark',
+      last: 'Otto',
+      handle: '@mdo',
+      collapsed: true,
+      masterDetail: [{ orderId: 1, orderDate: '24-07-1996', adress: '35 King George' }],
+    },
+    {
+      id: 2,
+      first: 'Jacob',
+      last: 'Thornton',
+      handle: '@fat',
+      collapsed: false,
+      masterDetail: [{ orderId: 2, orderDate: '04-01-1992', adress: 'Obere Str. 57' }],
+    },
+    {
+      id: 3,
+      first: 'Larry',
+      last: 'the Bird',
+      handle: '@twitter',
+      collapsed: false,
+      masterDetail: [{ orderId: 3, orderDate: '15-01-1994', adress: 'Kirchgasse 6' }],
+    },
+  ];
+  headElements = ['id', 'first', 'last', 'handle'];
+
+  masterHeadElements = ['Order Id', 'Order Date', 'Adress'];
+
+  searchText: string = '';
+  previous: string;
+
+  maxVisibleItems: number = 8;
+
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  @HostListener('input') oninput() {
+    this.mdbTablePagination.searchText = this.searchText;
+  }
+
+  ngOnInit() {
+    
+
+    this.mdbTable.setDataSource(this.elements);
+    this.elements = this.mdbTable.getDataSource();
+    this.previous = this.mdbTable.getDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.mdbTablePagination.setMaxVisibleItemsNumberTo(this.maxVisibleItems);
+
+    this.mdbTablePagination.calculateFirstItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+    this.cdRef.detectChanges();
+  }
+
+  addNewRow() {
+    this.mdbTable.addRow({
+      id: this.elements.length.toString(),
+      first: 'Wpis ' + this.elements.length,
+      last: 'Last ' + this.elements.length,
+      handle: 'Handle ' + this.elements.length
+    });
+    this.emitDataSourceChange();
+  }
+
+  addNewRowAfter() {
+    this.mdbTable.addRowAfter(1, {id: '2', first: 'Nowy', last: 'Row', handle: 'Kopytkowy'});
+    this.mdbTable.getDataSource().forEach((el: any, index: any) => {
+      el.id = (index + 1).toString();
+    });
+    this.emitDataSourceChange();
+  }
+
+  removeLastRow() {
+    this.mdbTable.removeLastRow();
+    this.emitDataSourceChange();
+    this.mdbTable.rowRemoved().subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+  removeRow() {
+    this.mdbTable.removeRow(1);
+    this.mdbTable.getDataSource().forEach((el: any, index: any) => {
+      el.id = (index + 1).toString();
+    });
+    this.emitDataSourceChange();
+    this.mdbTable.rowRemoved().subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+  emitDataSourceChange() {
+    this.mdbTable.dataSourceChange().subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+  searchItems() {
+    const prev = this.mdbTable.getDataSource();
+
+    if (!this.searchText) {
+      this.mdbTable.setDataSource(this.previous);
+      this.elements = this.mdbTable.getDataSource();
+    }
+
+    if (this.searchText) {
+      this.elements = this.mdbTable.searchLocalDataBy(this.searchText);
+      this.mdbTable.setDataSource(prev);
+    }
+
+    this.mdbTablePagination.calculateFirstItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+
+    this.mdbTable.searchDataObservable(this.searchText).subscribe(() => {
+      this.mdbTablePagination.calculateFirstItemIndex();
+      this.mdbTablePagination.calculateLastItemIndex();
+    });
+  }
+
+
+
+  downloadFile(data: any) {
+    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+    csv.unshift(header.join(','));
+    let csvArray = csv.join('\r\n');
+
+    var a = document.createElement('a');
+    var blob = new Blob([csvArray], {type: 'text/csv' }),
+    url = window.URL.createObjectURL(blob);
+
+    a.href = url;
+    a.download = "myFile.csv";
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+}
+
+
+
+}
